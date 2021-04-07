2316ac42b28bf16aa18eefdfed25dbf1fbbb9f6a
Dmitry A. Koshelev <necromancer@inbox.ru>
2021-04-07 20:45:48 +0300

    Main commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        items: [
+            'Home',
+            'Blog',
+            'About',
+            'Contacts',
+        ],
+        show: false,
+    },
+    
+});
